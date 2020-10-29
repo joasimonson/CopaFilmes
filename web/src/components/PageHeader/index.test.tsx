9a8fbf8864b02db7b10d29efbfb4b4aa4f8a7a46
joasimonson <joasimonson@hotmail.com>
2020-10-29 20:59:05 -0300

    implementação de testes unitários

---
+++ web/src/components/PageHeader/index.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import PageHeader from './index';
+
+describe('Testando renderização de Header', () => {
+    const componentTitulo = 'page-header-titulo';
+    const componentDescricao = 'page-header-descricao';
+
+    test('Renderização padrão de header', () => {
+        //Arrange
+        const titulo = "Título do header";
+        const descricao = "Descrição do header";
+        
+        //Act
+        const { getByTestId } = render(<PageHeader titulo={titulo} descricao={descricao} />);
+        const elTitulo = getByTestId(componentTitulo);
+        const elDescricao = getByTestId(componentDescricao);
+
+        //Assert
+        expect(elTitulo).toBeInTheDocument();
+        expect(elTitulo).toHaveTextContent(titulo);
+
+        expect(elDescricao).toBeInTheDocument();
+        expect(elDescricao).toHaveTextContent(descricao);
+    });
+
+    test('Renderização de header sem descrição', () => {
+        //Arrange
+        const titulo = "Título do header";
+        const descricao = "";
+        
+        //Act
+        const { queryByTestId } = render(<PageHeader titulo={titulo} descricao={descricao} />);
+        const elDescricao = queryByTestId(descricao);
+
+        //Assert
+        expect(elDescricao).not.toBeInTheDocument();
+    });
+});
---
+++ web/src/pages/SelecaoFilmes/index.test.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import { mocked } from 'ts-jest/utils';
+
+import SelecaoFilmes from './index';
+
+import { renderAll, waitForElementToBeRemoved, fireEvent, within, waitFor } from '../../tests/custom-render';
+import * as FilmeService from '../../services/FilmeService';
+import { filmesResponse } from '../../tests/FilmesMock';
+
+jest.mock('../../services/FilmeService');
+
+describe('Testando renderização da seleção de filmes', () => {
+    const componentLoading = 'component-loading';
+    const componentListaFilmes = 'lista-filmes';
+    const componentCardFilme = 'card-filme';
+    const componentCardCheckbox = 'card-filme-checkbox';
+    const componentTotalFilmesSelecionados = 'total-filmes-selecionados';
+    const componentTotalFilmesMax = 'total-filmes-max';
+
+    beforeEach(() => {
+        mocked(FilmeService.obterFilmes).mockImplementation(() => Promise.resolve(filmesResponse));
+    });
+
+    afterEach(() => {
+        mocked(FilmeService.obterFilmes).mockRestore();
+    });
+    
+    test('Testando se a chamada da API está sendo feita corretamente', async () => {
+        //Arrange
+        const { getByTestId } = renderAll(<SelecaoFilmes />);
+
+        //Act
+        await waitForElementToBeRemoved(() => getByTestId(componentLoading));
+
+        //Assert
+        expect(FilmeService.obterFilmes).toHaveBeenCalledTimes(1);
+    });
+
+    test('Validar exibição da lista de filmes', async () => {
+        //Arrange
+        const { getByTestId } = renderAll(<SelecaoFilmes />);
+
+        //Act
+        await waitForElementToBeRemoved(() => getByTestId(componentLoading));
+
+        //Assert
+        expect(getByTestId(componentListaFilmes)).toBeInTheDocument();
+    });
+
+    test('Validar se a quantidade de filmes presente na lista é igual a que veio externa', async () => {
+        //Arrange
+        const { getByTestId, getAllByTestId } = renderAll(<SelecaoFilmes />);
+
+        //Act
+        await waitForElementToBeRemoved(() => getByTestId(componentLoading));
+
+        const listCardFilme = getAllByTestId(componentCardFilme);
+        
+        //Assert
+        expect(listCardFilme).toHaveLength(filmesResponse.length);
+    });
+
+    test('Validar se a quantidade de filmes máxima permitida é menor ou igual a quantidade disponível', async () => {
+        //Arrange
+        const { getByTestId, getAllByTestId } = renderAll(<SelecaoFilmes />);
+
+        //Act
+        await waitForElementToBeRemoved(() => getByTestId(componentLoading));
+        
+        const elListaCardFilme = getAllByTestId(componentCardFilme);
+        const elTotalFilmesMax = getByTestId(componentTotalFilmesMax);
+        const totalFilmesMax = parseInt(elTotalFilmesMax.textContent as string);
+        
+        //Assert
+        expect(elTotalFilmesMax).toBeInTheDocument();
+        expect(totalFilmesMax).toBeLessThanOrEqual(elListaCardFilme.length);
+    });
+
+    test('Validar se a quantidade selecionada é maior que a permitida', async () => {
+        //Arrange
+        jest.spyOn(global, 'alert').mockImplementation(() => jest.fn());
+
+        const { getByTestId, getAllByTestId } = renderAll(<SelecaoFilmes />);
+
+        //Act
+        await waitForElementToBeRemoved(() => getByTestId(componentLoading));
+
+        const elListaCardFilme = getAllByTestId(componentCardFilme);
+        const elTotalFilmesMax = getByTestId(componentTotalFilmesMax);
+        const totalFilmesMaxMaiorQuePermitido = parseInt(elTotalFilmesMax.textContent as string) + 1;
+
+        for (let i = 0; i < totalFilmesMaxMaiorQuePermitido; i++) {
+            const filmeElement = elListaCardFilme[i];
+            
+            const checkBox = within(filmeElement).getByTestId(componentCardCheckbox);
+            
+            fireEvent.click(checkBox);
+
+            await waitFor(() => getByTestId(componentTotalFilmesSelecionados));
+        }
+
+        //Assert
+        expect(global.alert).toBeCalledWith('Atenção! O total máximo de filmes já foi selecionado.');
+    });
+
+    test('Validar seleção de filmes para disputa do campeonato', async () => {
+        //Arrange
+        const { getByTestId, getAllByTestId } = renderAll(<SelecaoFilmes />);
+
+        //Act
+        await waitForElementToBeRemoved(() => getByTestId(componentLoading));
+        
+        const elListaCardFilme = getAllByTestId(componentCardFilme);
+        const elTotalFilmesSelecionados = getByTestId(componentTotalFilmesSelecionados);
+        const elTotalFilmesMax = getByTestId(componentTotalFilmesMax);
+        const totalFilmesMax = parseInt(elTotalFilmesMax.textContent as string);
+
+        for (let i = 0; i < totalFilmesMax; i++) {
+            const filmeElement = elListaCardFilme[i];
+            
+            const checkBox = within(filmeElement).getByTestId(componentCardCheckbox);
+            
+            fireEvent.click(checkBox);
+
+            await waitFor(() => getByTestId(componentTotalFilmesSelecionados));
+        }
+        
+        //Assert
+        expect(elTotalFilmesMax).toBeInTheDocument();
+
+        expect(elTotalFilmesSelecionados).toBeInTheDocument();
+        expect(elTotalFilmesSelecionados).toHaveTextContent(elTotalFilmesMax.textContent as string);
+    });
+});
