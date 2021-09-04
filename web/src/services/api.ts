451c408cd6ee483e75fc2d1bc2d52d3e43c6f06e
joasimonson <joasimonson@hotmail.com>
2021-09-04 16:31:02 -0300

    correção code smell

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -7,18 +7,18 @@ const api = axios.create({
 
 export const getAuth = async (url: string) : Promise<AxiosResponse> => {
     async function get(config: any) {
-        return await api.get(url, config);
+        return api.get(url, config);
     }
 
-    return await enviarComRetentativa(get);
+    return enviarComRetentativa(get);
 }
 
 export const postAuth = async (url: string, data: any) : Promise<AxiosResponse> => {
     async function post(config: any) {
-        return await api.post(url, data, config)
+        return api.post(url, data, config)
     }
 
-    return await enviarComRetentativa(post);
+    return enviarComRetentativa(post);
 }
 
 const enviarComRetentativa = async (execute: any) : Promise<AxiosResponse> => {
